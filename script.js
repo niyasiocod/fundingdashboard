36e3e19b0619f280e722540b22fd940775ddf65a
Vidhuprasad-iocod <162094498+Vidhuprasad-iocod@users.noreply.github.com>
2024-03-27 19:53:58 +0530

    toggle btn activated

--- script.js
+++ script.js
@@ -1,15 +1,51 @@
-const navItems = document.getElementById('navItems');
+const navSettingsItems = document.getElementById('navSettingsItems');
 const arrowUp = document.getElementById('arrowUp');
 const arrowDown = document.getElementById('arrowDown');
 
+
+const navbar = document.getElementsByClassName('navbar')[0];
+const navbarItems = document.getElementsByClassName('navbar-items')[0];
+const navbarTitle = document.getElementsByClassName('navbar-title');
+const main = document.getElementsByTagName('main')[0];
+const minimizerLogoContainer = document.getElementsByClassName('minimizer-logo-container')[0];
+const arrowContainer = document.getElementsByClassName('arrow-container')[0];
+
+
+let n=1;
+const navToggle = () => {
+    navSettingsItems.classList==='active' ? navSettingsItems.classList.remove("active") : navSettingsItems.classList.remove("active");
+
+    if(n%2===0){
+        minimizerLogoContainer.classList.remove("navToggle");
+        navbar.classList.remove("navToggle");
+        navbarItems.classList.remove("navToggle");
+        main.classList.remove("navToggle");
+        arrowContainer.classList.remove("navToggle");
+        for(let i =0; i<navbarTitle.length; i++){
+            navbarTitle[i].classList.remove("navToggle");
+        }
+    }else{
+        minimizerLogoContainer.classList.add("navToggle");
+        navbar.classList.add("navToggle");
+        navbarItems.classList.add("navToggle");
+        main.classList.add("navToggle");
+        arrowContainer.classList.add("navToggle");
+        for(let i =0; i<navbarTitle.length; i++){
+            navbarTitle[i].classList.add("navToggle");
+        }
+    }
+    
+    n++;
+}
+
 const visibleItem = () => {
-    navItems.classList.add("active");
+    navSettingsItems.classList.add("active");
     arrowUp.classList.add("active");
     arrowDown.classList.add("active");
 }
 
 const disableItem = () => {
-    navItems.classList.remove("active");
+    navSettingsItems.classList.remove("active");
     arrowUp.classList.remove("active");
     arrowDown.classList.remove("active");
 }
